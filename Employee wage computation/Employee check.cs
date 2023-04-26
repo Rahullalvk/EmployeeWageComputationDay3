222f0ea4ad4b3fa666f2969a9d405f0162a348a6
Rahulalvk <rahulal432@gmail.com>
2023-04-25 20:44:07 -0700

    Added employee wage program

--- Employee wage computation/Employee check.cs
+++ Employee wage computation/Employee check.cs
@@ -10,18 +10,24 @@ namespace Employee_wage_computation
     {
         public static void EmployeeAttendence()
         {
+            int EMP_RATE_PER_HOUR = 20;
 
             int IS_FULL_TIME = 1;
+        
+            int empHours = 0;
+            int empWages = 0;
             Random rdm = new Random();
             int empCheck = rdm.Next(2);
             if(empCheck == IS_FULL_TIME) 
             {
-                Console.WriteLine("Employee Is present");
+                empHours = 8;
             }
             else 
             {
-                Console.WriteLine("Employee Is absent");
+                empHours = 0;
             }
+            empWages = empHours * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total wage of the employee :"+ empWages);
        }
     }
 }
