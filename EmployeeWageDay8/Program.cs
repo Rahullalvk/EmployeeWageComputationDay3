58040a3949178e35a094dd706533aa34781a9151
Rahullal <Rahullal432@gmail.com>
2023-05-07 11:28:24 +0530

    Code to Check Employee is Present or Absent

---
+++ EmployeeWageDay8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageDay8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.EmployeeAttendence();
+        }
+    }
+}
